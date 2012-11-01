112a2d0b790753bd15aaff02aff9a135956d18c8
Justin Bruce Van Horne <justinvh@gmail.com>
2012-10-31 22:10:56 -0700

    Adds support for rendering tiles.
    - Added Tile constructs and Layer constructs
    - Added basic frag and vert shaders.
    - Fixed Rectangle to work on float()
    - Added left, right, top, bottom methods to Rect.

--- src/edu/siren/core/Rectangle.java
+++ src/edu/siren/core/Rectangle.java
@@ -1,10 +1,29 @@
 package edu.siren.core;
 
-public class Rectangle extends java.awt.Rectangle {
-    private static final long serialVersionUID = 5995669794424202731L;
+public class Rectangle {
+    float x, y, width, height;
 
-    Rectangle(int x, int y) {
-        super(x, y);
+    public Rectangle(float x, float y, float w, float h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+    }
+
+    public float left() {
+        return x;
+    }
+
+    public float right() {
+        return x + width;
+    }
+
+    public float top() {
+        return y;
+    }
+
+    public float bottom() {
+        return y + height;
     }
 
     void extend(Rectangle other) {
