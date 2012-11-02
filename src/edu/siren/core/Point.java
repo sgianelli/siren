53baba78dfc0ff542184f498bfa685fc1e7c6c1f
George Amaya <gamaya@email.arizona.edu>
2012-11-02 10:11:50 -0700

    Created Base Unit and Sprite

---
+++ src/edu/siren/core/Point.java
@@ -0,0 +1,59 @@
+package edu.siren.core;
+
+import java.io.Serializable;
+
+/**
+ * The Location of a Unit on the Map
+ * 
+ * @author The Java Throwers
+ *
+ */
+public class Point implements Serializable {
+
+	// Serial ID
+	private static final long serialVersionUID = 1L;
+	
+	// Point Location
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	/**
+	 * Get the X Location 
+	 * 
+	 * @return int
+	 */
+	public int getXLocation() {
+		return this.x;
+	}
+	
+	/**
+	 * Get the Y Location
+	 * 
+	 * @return int
+	 */
+	public int getYLocation() {
+		return this.y;
+	}
+	
+	/**
+	 * Set the x Location
+	 */
+	public void setXLocation(int x) {
+		this.x = x;
+	}
+	
+	/**
+	 * Set the y location
+	 */
+	public void setYLocation(int y) {
+		this.y  = y;
+	}
+	
+}
---
+++ src/edu/siren/unit/BaseUnit.java
@@ -0,0 +1,47 @@
+package edu.siren.unit;
+
+import java.io.Serializable;
+
+import edu.siren.core.Point;
+
+/**
+ * The Base Unit Class
+ * 
+ * @author Java Throwers
+ *
+ */
+public abstract class BaseUnit implements Serializable {
+
+	// The Current Location of a Unit
+	private Point currentLocation;
+	
+	// Serial ID
+	private static final long serialVersionUID = -3978890929306129878L;
+
+	/**
+	 * Create a Base Unit Class with a Location
+	 * 
+	 */
+	public BaseUnit(Point location) {
+		this.currentLocation = location;
+	}
+	
+	/**
+	 * Get the Current location of a Sprit
+	 * 
+	 */
+	public Point getCurrentLocation() {
+		return this.currentLocation;
+	}
+	
+	/**
+	 * The Sprite has the ability to commicate with
+	 * other sprites
+	 * 
+	 * @return
+	 */
+	public abstract boolean canCommunicate();
+	
+	public abstract boolean isMagical();
+	
+}
