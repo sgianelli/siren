8083e621f0be9a5bb79a96b67740bab54c5d44e5
Timmy Garrabrant <luigiinred@gmail.com>
2012-12-04 17:56:32 -0700

    Delete profiles

--- src/edu/siren/game/items/AttackPotion.java
+++ src/edu/siren/game/items/AttackPotion.java
@@ -10,7 +10,7 @@ public class AttackPotion extends Item  {
 	public AttackPotion(Profile profile) {
 		super("Attack Potion", profile);
 		// TODO Auto-generated constructor stub
-		Icon icon = createImageIcon("res/game/","");
+		//Icon icon = createImageIcon("res/game/","");
 //		
 //		this.setIcon(icon);
 	}
